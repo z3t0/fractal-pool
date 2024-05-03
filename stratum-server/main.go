99618c5c4e0cbf91df9a04339abcd5ea7062cb5c
Rafi Khan <rafi@rafikhan.io>
2024-05-02 21:43:26 -0400

    add tcp server
    https://github.com/fractal-pool/fractal-pool/issues/5

--- stratum-server/main.go
+++ stratum-server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
 
 func main() {
 	fmt.Println("Starting Fractal Pool...")
+
+	listener, err := net.Listen("tcp", ":5000")
+
+	if err != nil {
+		fmt.Println("Error listening on port 5000: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Listening on 0.0.0.0:5000")
+
+	for {
+		conn, err := listener.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting tcp conn:", err.Error())
+			continue
+		}
+
+		go handleTcpConnection(conn)
+	}
+}
+
+func handleTcpConnection(conn net.Conn) {
+	fmt.Println("Received tcp connection")
+
+	conn.Close()
 }
